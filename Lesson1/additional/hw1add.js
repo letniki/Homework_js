029ca23006e11cdd946fddc0ceacc8dc8e3b3b9a
letniki <letutanikita12@gmail.com>
2024-04-29 22:15:19 +0300

    create lesson1

---
+++ Lesson1/additional/hw1add.js
@@ -0,0 +1,6 @@
+let firstname= prompt('first name');
+console.log(firstname);
+let middlename= prompt('middle name');
+console.log(middlename);
+let age=prompt('age');
+console.log(age);
---
+++ Lesson1/hw1_2.js
@@ -0,0 +1,6 @@
+let firstName= 'nikita';
+let lastName= 'letuta';
+let middleName='valeriyovych';
+let space = ' ';
+let person=lastName + space + firstName + space + middleName;
+console.log(person);
