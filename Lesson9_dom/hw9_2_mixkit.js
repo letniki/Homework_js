17dcb268323ec11f65dd09dc44f85ac6dccee42f
letniki <letutanikita12@gmail.com>
2024-05-23 14:45:35 +0300

    update hw9_2_mixkit.js

--- Lesson9_dom/hw9_2_mixkit.js
+++ Lesson9_dom/hw9_2_mixkit.js
@@ -1,7 +1,13 @@
-// пошук всіх авторів
-
 let authorNode = document.querySelectorAll('.item-grid-music-preview__author');
-for(const auth of authorNode){
-    let author=auth.innerText.substring(auth.innerText.indexOf('y')+2,auth.innerText.length)
-    ;console.log(author);
+let boxNode = document.querySelectorAll('.item-grid-card__meta');
+let titleNode = document.querySelectorAll('.item-grid-card__title');
+let musicNode = document.querySelectorAll('div.audio-player--music audio');
+let arr=[];
+for (let i = 0; i < boxNode.length; i++) {
+    let obj = {};
+    obj['title'] = titleNode[i].innerText;
+    obj['author'] = authorNode[i].innerText.substring(authorNode[i].innerText.indexOf('y') + 2);
+    obj['mp3']= musicNode[i].currentSrc;
+    arr.push(obj);
 }
+console.log(arr);
